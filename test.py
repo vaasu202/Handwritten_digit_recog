226ab6c6b44772e47fe7a070884f2b126780eb30
vaasu202 <71887810+vaasu202@users.noreply.github.com>
2022-12-02 09:48:38 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+class Testhello(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status,'200 OK')
+        self.assertEqual(rv.data,b"HELLO WORLD \n")
+
+if __name__ == '__main__':
+    unittest.main()
